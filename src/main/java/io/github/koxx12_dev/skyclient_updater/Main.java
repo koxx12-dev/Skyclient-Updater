cffc86ccecb78673f5840c6ed3b3580f97c34525
koxx12-dev <80456351+koxx12-dev@users.noreply.github.com>
2021-06-06 16:24:21 +0200

    stuff

---
+++ src/main/java/io/github/koxx12_dev/skyclient_updater/Main.java
@@ -0,0 +1,7 @@
+package io.github.koxx12_dev.skyclient_updater;
+
+public class Main extends Utils {
+    public static void main(String[] args) {
+        sendLog("test",Main.class,LogType.INFO);
+    }
+}
